d4fa8416c5ba44be86db38f4be60529a8c9c58ed
Daniel Navarrete <danielnavarrete095@gamil.com>
2022-02-20 17:36:34 -0700

    Changed to keypressed event, still need to implement double key press

--- etch-a-sketch/main.py
+++ etch-a-sketch/main.py
@@ -19,15 +19,15 @@ def clear_screen():
 
 def set_key_bindings():
     screen.listen()
-    screen.onkey(key="w", fun=move_forwards)
-    screen.onkey(key="Up", fun=move_forwards)
-    screen.onkey(key="s", fun=move_backwards)
-    screen.onkey(key="Down", fun=move_backwards)
-    screen.onkey(key="a", fun=turn_ccw)
-    screen.onkey(key="Left", fun=turn_ccw)
-    screen.onkey(key="d", fun=turn_cw)
-    screen.onkey(key="Right", fun=turn_cw)
-    screen.onkey(key="c", fun=clear_screen)
+    screen.onkeypress(key="w", fun=move_forwards)
+    screen.onkeypress(key="Up", fun=move_forwards)
+    screen.onkeypress(key="s", fun=move_backwards)
+    screen.onkeypress(key="Down", fun=move_backwards)
+    screen.onkeypress(key="a", fun=turn_ccw)
+    screen.onkeypress(key="Left", fun=turn_ccw)
+    screen.onkeypress(key="d", fun=turn_cw)
+    screen.onkeypress(key="Right", fun=turn_cw)
+    screen.onkeypress(key="c", fun=clear_screen)
     
 def main():
     set_key_bindings()
