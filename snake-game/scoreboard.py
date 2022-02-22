f53455638d198d3524765019133b98540b3a72f5
Daniel Navarrete <danielnavarrete095@gamil.com>
2022-02-22 16:05:51 -0700

    Add high score persistence, it is saved into a json file

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -1,12 +1,13 @@
 from turtle import Turtle
-
+import json
+HIGH_SCORE_F = "high_score.json"
 ALIGNMENT = "center"
 FONT = "Calibri", 15, "bold"
 class Scoreboard(Turtle):
     def __init__(self, y):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.text = f"Score: {self.score}  High Score: {self.high_score}"
         self.hideturtle()
         self.pu()
@@ -29,4 +30,17 @@ class Scoreboard(Turtle):
         self.clear()
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_high_score()
         self.score = 0
+    
+    def save_high_score(self):
+        highscore = {"highscore": self.high_score}
+        json_string = json.dumps(highscore)
+        print(json_string)
+        with open(HIGH_SCORE_F, "w") as file:
+            file.write(json_string)
+    
+    def read_high_score(self):
+        with open(HIGH_SCORE_F, "r") as file:
+            highscore = json.load(file)
+        return highscore["highscore"]
