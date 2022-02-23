54dd30f06c613232df708df5aaba4fb8d049028d
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-02-23 13:09:31 -0700

    Add exit mechanism, when it exits it also saves a csv with the states you need to learn

--- us-states-quizz/main.py
+++ us-states-quizz/main.py
@@ -20,30 +20,34 @@ def main():
 
     # Get data from csv file
     data = pandas.read_csv(DATA_FILE)
-    data_dict = data.to_dict()
-    # print(data_dict)
     data_states_list = data.state.to_list()
-    print(data_states_list[0])
 
-    game_is_on = True
     c = turtle.getcanvas()
     right_guesses = []
+    missing_states = []
     score = 0
-    while(game_is_on):
-        answer = screen.textinput(title="Guess the state", prompt="Guess another state's name")
-        screen.listen()
-        state = answer.lower()
-        for i, state in enumerate(data_states_list):
-            if answer.lower() == state.lower() :
-                # Write name on the right coordinates on the map
-                x = int(data[data.state == state].x)
-                # Y is inverted in canvas coordinate system
-                y = - int(data[data.state == state].y)
-                c.create_text(x, y, text=state, angle=0, font=(FONT, FONT_SIZE, FONT_TYPE))
-                right_guesses.append(state)
-                score +=1
-                print(f"Score: {score}")
-    turtle.mainloop()
+    while(len(right_guesses) < 50):
+        answer = screen.textinput(title=f"{len(right_guesses)}/50 States Correct", prompt="Guess another state's name").title()
+        state = answer
+        if answer == "Exit" or answer == "E":
+            # generate states_to_learn.csv, contains states which haven't been guessed
+            for state in data_states_list:
+                if state not in right_guesses:
+                    missing_states.append(state)
+            print(missing_states)
+            df = pandas.DataFrame(missing_states)
+            print(df)
+            df.to_csv("states_to_learn.csv")
+            break
+        if answer in data_states_list:
+            # Write name on the right coordinates on the map
+            x = int(data[data.state == state].x)
+            # Y is inverted in canvas coordinate system
+            y = - int(data[data.state == state].y)
+            c.create_text(x, y, text=state, angle=0, font=(FONT, FONT_SIZE, FONT_TYPE))
+            right_guesses.append(state)
+            score +=1
+            print(f"Score: {score}")
 
 def get_mouse_click_coor(x, y):
     print(x, y)
