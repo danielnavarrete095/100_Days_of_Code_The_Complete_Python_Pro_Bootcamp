d3a882c970e2d1a96ea0db7dc2b13e9fbf53c054
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-02-25 11:56:16 -0700

    Word and translation change(randomly) every time a button is clicked

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,8 +1,52 @@
 from tkinter import *
-
+from pandas import read_csv, DataFrame
+from random import randint, choice
 BACKGROUND_COLOR = "#B1DDC6"
+FLASH_CARD_IMAGE = r"images\card_front.png"
+CHECK_IMAGE = r"images\right.png"
+CROSS_IMAGE = r"images\wrong.png"
+GERMAN_WORDS_FILE = r"data\german_words.csv"
+FONT = "Ariel"
 
+word_text = ""
+translation_text = ""
+flash_card_canvas = None
+
+def get_random_word():
+    global word_text, translation_text, flash_card_canvas
+    word, translation = choice(list(data_dict.items()))
+    print(translation)
+    print(word)
+    # return (word, translation)
+    flash_card_canvas.itemconfig(word_text, text=word)
+    flash_card_canvas.itemconfig(translation_text, text=translation)
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# flash card canvas
+flash_card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+flash_card_canvas.grid(column=0, row=0, columnspan=2)
+flash_card_img = PhotoImage(file = FLASH_CARD_IMAGE)
+flash_card_canvas.create_image(400, 263, image=flash_card_img)
+word_text = flash_card_canvas.create_text(400, 150, text="Language", fill="black", font=(FONT, 40, "italic"))
+translation_text = flash_card_canvas.create_text(400, 263, text="Word", fill="black", font=(FONT, 60, "bold"))
+
+# buttons
+check_img = PhotoImage(file=CHECK_IMAGE)
+check_button = Button(image=check_img, highlightthickness=0, borderwidth=0, command=get_random_word)
+check_button.grid(column=0, row=1)
+cross_img = PhotoImage(file=CROSS_IMAGE)
+cross_button = Button(image=cross_img, highlightthickness=0, borderwidth=0, command=get_random_word)
+cross_button.grid(column=1, row=1)
+
+
+# Read csv file
+data = read_csv(GERMAN_WORDS_FILE)
+# print(data)
+data_dict = {row.German:row.English for (index, row) in data.iterrows()}
+# print(data_dict)
+
 
 window.mainloop()
