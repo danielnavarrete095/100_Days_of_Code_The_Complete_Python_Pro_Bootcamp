97e1e627bebce27a28efdb6665f1d84dc6a44f6c
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-02-25 16:44:51 -0700

    Sending mail from outlook and gmail working

--- smpt-project/main.py
+++ smpt-project/main.py
@@ -2,14 +2,21 @@ import smtplib
 
 from login_info import *
 
-SMTP_HOTMAIL = "smtp.live.com"
+# SMTP_HOTMAIL = "smtp.live.com"
+SMTP_HOTMAIL = "smtp.office365.com"
 SMTP_GMAIL = "smtp.gmail.com"
-# smtp.office365.com
+SMTP_PORT = 587
 
-connection = smtplib.SMTP(SMTP_GMAIL)
+# with smtplib.SMTP(SMTP_GMAIL, GMAIL_PORT) as connection:
 
-connection.starttls()
-connection.login(user=GMAIL_USER, password=GMAIL_PASSWORD)
+#     connection.starttls()
+#     connection.login(user=GMAIL_USER, password=GMAIL_PASSWORD)
 
-connection.sendmail(from_addr=GMAIL_USER, to_addrs=HOTMAIL_USER, msg="Hello world! \nSending mail from python!")
-connection.close()
+#     connection.sendmail(from_addr=GMAIL_USER, to_addrs=HOTMAIL_USER, msg="Subject:Hello\n\nHello world! \nSending mail from python!")
+    
+with smtplib.SMTP(SMTP_HOTMAIL, PORT) as connection:
+
+    connection.starttls()
+    connection.login(user=HOTMAIL_USER, password=HOTMAIL_PASSWORD)
+
+    connection.sendmail(from_addr=HOTMAIL_USER, to_addrs=GMAIL_USER, msg="Subject:Hello\n\nHello world! \nSending mail from python!")
