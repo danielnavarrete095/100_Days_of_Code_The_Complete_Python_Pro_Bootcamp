a7459cb5d7bf137978e4a90371853eee4505edd4
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-02-25 12:43:01 -0700

    Fix texts, the card should show the language and the word Add flip card funtcion to show the back of the card after 3 seconds

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,29 +1,51 @@
+from textwrap import fill
 from tkinter import *
+from numpy import flip
 from pandas import read_csv, DataFrame
 from random import randint, choice
 BACKGROUND_COLOR = "#B1DDC6"
 FLASH_CARD_IMAGE = r"images\card_front.png"
+FLIP_CARD_IMAGE = r"images\card_back.png"
 CHECK_IMAGE = r"images\right.png"
 CROSS_IMAGE = r"images\wrong.png"
 GERMAN_WORDS_FILE = r"data\german_words.csv"
 FONT = "Ariel"
+ORIGIN_LANGUAGE =  "German"
+TRANSLATION_LANGUAGE = "English"
 
 word_text = ""
 translation_text = ""
 flash_card_canvas = None
+current_word = ""
+current_translation = ""
+front_card_image = None
+back_card_image = None
 
 def get_random_word():
-    global word_text, translation_text, flash_card_canvas
+    global word_text, current_word, current_translation
     # word, translation = choice(list(data_dict.items()))
     word = choice(data_dict)
     # print(translation)
     print(word)
-    return (word["German"], word["English"])
+    current_word = word["German"]
+    current_translation = word["English"]
+    # return (word["German"], word["English"])
 
 def next_card():
-    word, translation = get_random_word()
-    flash_card_canvas.itemconfig(word_text, text=word)
-    flash_card_canvas.itemconfig(translation_text, text=translation)
+    global flash_card_canvas, current_word, front_card_img
+    # word, translation = get_random_word()
+    get_random_word()
+    flash_card_canvas.itemconfig(image_container,image=front_card_img)
+    flash_card_canvas.itemconfig(word_text, text=ORIGIN_LANGUAGE, fill = "black")
+    flash_card_canvas.itemconfig(translation_text, text=current_word, fill = "black")
+    flash_card_canvas.after(3000, flip_card)
+
+def flip_card():
+    global flash_card_canvas, current_translation, back_card_img
+    flash_card_canvas.itemconfig(image_container,image=back_card_img)
+    flash_card_canvas.itemconfig(word_text, text=TRANSLATION_LANGUAGE, fill = "white")
+    flash_card_canvas.itemconfig(translation_text, text=current_translation, fill = "white")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -32,9 +54,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # flash card canvas
 flash_card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flash_card_canvas.grid(column=0, row=0, columnspan=2)
-flash_card_img = PhotoImage(file = FLASH_CARD_IMAGE)
-flash_card_canvas.create_image(400, 263, image=flash_card_img)
-word_text = flash_card_canvas.create_text(400, 150, text="Language", fill="black", font=(FONT, 40, "italic"))
+front_card_img = PhotoImage(file = FLASH_CARD_IMAGE)
+back_card_img = PhotoImage(file = FLIP_CARD_IMAGE)
+image_container = flash_card_canvas.create_image(400, 263, image=front_card_img)
+word_text = flash_card_canvas.create_text(400, 150, text=ORIGIN_LANGUAGE, fill="black", font=(FONT, 40, "italic"))
 translation_text = flash_card_canvas.create_text(400, 263, text="Word", fill="black", font=(FONT, 60, "bold"))
 # buttons
 check_img = PhotoImage(file=CHECK_IMAGE)
