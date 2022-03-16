929c70cfd8ef3cf05d2248f3fecb5dfd5a0b1814
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-03-16 14:01:16 -0700

    Add post put and delete requests

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,5 +1,8 @@
 import requests
 from keys import *
+from datetime import datetime, timedelta
+
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 req_params = {
@@ -12,3 +15,42 @@ req_params = {
 # response = requests.post(pixela_endpoint, json=req_params)
 # response.raise_for_status()
 # print(response.text)
+
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+req_params = {
+    "id": GRAPH_ID,
+    "name": "Programming graph",
+    "unit": "Hr",
+    "type": "int",
+    "color": "kuro",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(graph_endpoint, json=req_params, headers=headers)
+# print(response.text)
+
+add_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now() - timedelta(1)
+# today = datetime(year=2022, month=2, day=15)
+
+req_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "1",
+}
+# response = requests.post(add_pixel_endpoint, json=req_params, headers=headers)
+# print(response.text)
+
+req_params = {
+    "quantity": "1",
+}
+update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.put(update_pixel_endpoint, json=req_params, headers=headers)
+print(response.text)
+
+delete_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(delete_pixel_endpoint, headers=headers)
+print(response.text)
+
