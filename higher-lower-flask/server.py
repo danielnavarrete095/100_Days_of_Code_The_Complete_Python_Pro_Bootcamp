04d875d3cc24612dd444d21aca8d95cd124f97a8
Daniel Navarrete <danielnavarrete095@gmail.com>
2022-03-30 14:05:48 -0700

    Add simple guess number game with flask

---
+++ higher-lower-flask/server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from random import randint
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    global random_num
+    random_num = randint(0, 9)
+    print(random_num)
+    return "<h1>Guess a number between 0 and 9</h1>"\
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+
+@app.route("/<int:num>")
+def guess(num):
+    if num > random_num:
+        return "<h1 style='color: purple;'>Too high, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
+    elif num < random_num:
+        return "<h1 style='color: red;'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
+    else:
+        return "<h1 style='color: green;'>You found me!</h1>"\
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
+
+if __name__ == "__main__":
+    random_num = randint(0, 9)
+    print(random_num)
+    app.run(debug=True)
